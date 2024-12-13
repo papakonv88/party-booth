7c2d54006b4951dd0e80ec364536c2a3e7e644e0
VasilisPapakonstantinou <v.papakonstantinou@ysoft.gr>
2024-12-13 15:34:43 +0200

    initial

--- components/ui/Testimonials/index.jsx
+++ components/ui/Testimonials/index.jsx
@@ -7,17 +7,16 @@ const Testimonials = () => {
             <div id="testimonials" className="max-w-screen-xl mx-auto px-4 md:px-8 mb-20">
                 <div className="max-w-4xl sm:text-center md:mx-auto">
                     <h2 className="text-gray-800 text-3xl font-semibold sm:text-4xl mb-10">
-                        Professional Photobooth Rental in Thessaloniki | SKG
+                     Επαγγελματικές Υπηρεσίες Ενοικίασης PhotoBooth στην Θεσσαλονίκη | SKG
                     </h2>
                     <p className="mt-3 text-gray-600">
-                        Capture unforgettable moments with our premium photobooth rental service in Thessaloniki and
-                        surrounding areas.
+                        Απαθανατίστε αξέχαστες στιγμές με την υπηρεσία ενοικίασης premium photobooth στη Θεσσαλονίκη και τις γύρω περιοχές.
                     </p>
                     <p className="mt-3 text-gray-600">
-                        Perfect for weddings, parties, and corporate events, we bring fun, style, and
-                        memories to your special occasions.I                    </p>
+                        Ιδανικό για γάμους, πάρτι και εταιρικές εκδηλώσεις, προσφέρουμε διασκέδαση, στυλ και αναμνήσεις στις ξεχωριστές σας στιγμές.
+                    </p>
                     <p className="mt-3 text-gray-600">
-                        Book now for a seamless experience that your guests will love!
+                        Κλείστε τώρα για μια μοναδική εμπειρία που οι καλεσμένοι σας θα λατρέψουν!
                     </p>
                 </div>
             </div>
