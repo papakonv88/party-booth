7eecd38d48366303fe6ee7c9d37b69f95147d9c2
VasilisPapakonstantinou <v.papakonstantinou@ysoft.gr>
2024-12-21 13:50:59 +0200

    url fix

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -1,20 +1,15 @@
-import { Resend } from 'resend';
+import {Resend} from 'resend';
 
 // Initialize Resend with your API key
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://www.party-booth.gr');
-    if (req.method === 'POST') {
-        const { fullName, contactNumber, email, location, date, message, services } = req.body;
-
-        try {
-            // Send the email
-            const data = await resend.emails.send({
-                from: `"Contact Form" <no-reply@resend.dev>`, // sender address
-                to: process.env.EMAIL_USER,
-                subject: "Photo Booth Email Request",
-                text: `New Message: 
+export default async (req, res) => {
+    const {fullName, contactNumber, email, location, date, message, services} = req.body;
+    // Send the email
+    const {data, error} = await resend.emails.send({
+        from: 'Contact Form <no-reply@resend.dev>', // sender address
+        to: process.env.EMAIL_USER,
+        subject: "Photo Booth Email Request",
+        text: `New Message: 
                      Name: ${fullName}
                      Contact Number: ${contactNumber}
                      Email: ${email}
@@ -22,14 +17,10 @@ export default async function handler(req, res) {
                      Date: ${date}
                      Message: ${message}
                      Services: ${services.join(", ")}`,
-            });
-            res.status(200).json({ success: true, data });
-        } catch (error) {
-            console.error('Error sending email:', error);
-            res.status(500).json({ success: false, error: error.message });
-        }
-    } else {
-        res.setHeader('Allow', ['POST']);
-        return res.status(405).end(`Method ${req.method} Not Allowed`);
+    });
+
+    if (error) {
+        return res.status(400).json(error);
     }
+    res.status(200).json({success: true, data});
 }
