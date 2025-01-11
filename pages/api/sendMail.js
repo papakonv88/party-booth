7eadd69a9007396566c17dd3bb99b1f4d0509444
Vasilis Papakonstantinou <vasilis.papakonstantinou@datawise.ai>
2025-01-11 18:15:19 +0200

    Fixxx

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -3,16 +3,7 @@ import {Resend} from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 export default async (req, res) => {
     console.log('hiii')
-    const {fullName, contactNumber, email, location, date, message, services} = req.body || {
-        fullName: 'test',
-        contactNumber: '1234',
-        email: 'sdds@sdsd.com',
-        location: 'fdfd',
-        date: 'ddsds',
-        message: 'sdsd',
-        services: ['sdsds', 'sdsdsd']
-    };
-/*
+    const {fullName, contactNumber, email, location, date, message, services} = req.body || {};
     const {data, error} = await resend.emails.send({
         from: 'Photo Booth | SKG <you@party-booth.gr>',
         to: process.env.EMAIL_USER,
@@ -26,7 +17,6 @@ export default async (req, res) => {
                      Message: ${message}
                      Services: ${services.join(", ")}`,
     });
-*/
 
     if (error) {
         return res.status(400).json(error);
