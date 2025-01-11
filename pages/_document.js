f80ca59b28689b85e755316ada31d2f541673a43
Vasilis Papakonstantinou <vasilis.papakonstantinou@datawise.ai>
2025-01-11 20:33:26 +0200

    GA4

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className='scroll-smooth' lang='en'>
+    <Html className='scroll-smooth' lang='el'>
         <Head>
+            {/* Google Analytics 4 Script */}
+            <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=G-TES2ZW04KM`}
+            ></script>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                
+                gtag('config', 'G-TES2ZW04KM');
+              `,
+                }}
+            />
+            {/* Favicon and Meta Tags */}
             <link rel="icon" href="/favicon.ico"/>
             <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
