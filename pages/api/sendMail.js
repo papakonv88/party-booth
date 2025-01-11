03760a5cd552aacdfb8257318df66581de7b792a
Vasilis Papakonstantinou <vasilis.papakonstantinou@datawise.ai>
2025-01-11 16:57:51 +0200

    Fix

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -2,10 +2,23 @@ import {Resend} from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 export default async (req, res) => {
+
+    if (req.method !== 'POST') {
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+
     console.log('hiii')
-    const {fullName, contactNumber, email, location, date, message, services} = req.body || {fullName: 'test', contactNumber: '1234', email: 'sdds@sdsd.com', location: 'fdfd', date: 'ddsds', message: 'sdsd', services: ['sdsds', 'sdsdsd']};
-/*    const {data, error} = await resend.emails.send({
-        from:  'Photo Booth | SKG <you@party-booth.gr>',
+    const {fullName, contactNumber, email, location, date, message, services} = req.body || {
+        fullName: 'test',
+        contactNumber: '1234',
+        email: 'sdds@sdsd.com',
+        location: 'fdfd',
+        date: 'ddsds',
+        message: 'sdsd',
+        services: ['sdsds', 'sdsdsd']
+    };
+    const {data, error} = await resend.emails.send({
+        from: 'Photo Booth | SKG <you@party-booth.gr>',
         to: process.env.EMAIL_USER,
         subject: "Photo Booth Email Request",
         text: `New Message: 
@@ -16,7 +29,7 @@ export default async (req, res) => {
                      Date: ${date}
                      Message: ${message}
                      Services: ${services.join(", ")}`,
-    });*/
+    });
 
     if (error) {
         return res.status(400).json(error);
